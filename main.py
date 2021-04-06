d9ffae0120a2486323f97f44dfd38d14f0e38b7a
cbenu4cse18036 <cb.en.u4cse18036@cb.students.amrita.edu>
2021-04-06 20:48:51 +0530

    project setup

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/insertion")
+def insert():
+    return render_template("insertion.html")
+
+@app.route("/deletion")
+def delete():
+    return render_template("deletion.html")  
+          
+if __name__ == "__main__":
+    app.run(debug=True)
